ea90b1d9a7bcedaff15d126abcb58cd3ff9bf646
seamus.bao <seamus.bao@bytedance.com>
2023-04-07 18:38:31 +0800

    -Drawing a triangle --step ---Instance

--- Project1/Project1/VulkanTest.cpp
+++ Project1/Project1/VulkanTest.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 #include <stdexcept>
 #include <cstdlib>
 
@@ -24,12 +25,87 @@ private:
 		glfwInit();//初始化 glfw 库
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);//不去创建 OpenGL context
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);//不处理窗口缩放
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//宽高加标题，不指定显示器，最后一个参数跟GL相关，不用管
 	}
 
 	void initVulkan()
 	{
+		createInstance();
+	}
 
+	void createInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t supportedExtensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, nullptr);
+		std::vector<VkExtensionProperties> supportedExtensions(supportedExtensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, supportedExtensions.data());
+
+		std::cout << "supported extension:\n";
+		for (const auto& extension : supportedExtensions)
+		{
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		std::vector<const char*> requiredExtensions;
+
+		for (uint32_t i = 0; i < glfwExtensionCount; i++)
+		{
+			requiredExtensions.emplace_back(glfwExtensions[i]);
+		}
+
+		requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+		
+		std::cout << "required extension:\n";
+		for (const auto& requiredEXT : requiredExtensions)
+		{
+			std::cout << '\t' << requiredEXT << '\n';
+		}
+
+		std::cout << "enabled extension:\n";
+		std::vector<const char*> enabledExtensions;
+		for (const auto& supportedEXT : supportedExtensions)
+		{
+			for (const auto& requiredEXT : requiredExtensions)
+			{
+				if (strcmp(supportedEXT.extensionName, requiredEXT) == 0)
+				{
+					enabledExtensions.emplace_back(requiredEXT);
+					if (requiredEXT == VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME)
+					{
+						createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+					}
+					std::cout << '\t' << requiredEXT << '\n';
+					break;
+				}
+			}
+		}
+
+		createInfo.enabledExtensionCount = (uint32_t)enabledExtensions.size();
+		createInfo.ppEnabledExtensionNames = enabledExtensions.data();
+
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+		if (result != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance");
+		}
 	}
 
 	void mainLoop()
@@ -42,11 +118,13 @@ private:
 
 	void cleanup()
 	{
+		vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 };
 
 int main()
